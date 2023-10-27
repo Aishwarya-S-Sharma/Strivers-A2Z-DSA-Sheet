d495fa89b95771e935d594df31e8c7075bf6c9fa
Aishwarya S <98091836+Aishwarya-S-Sharma@users.noreply.github.com>
2023-10-28 01:17:58 +0530

    Add files via upload

---
+++ Pattern/pattern9.py
@@ -0,0 +1,16 @@
+#print pyramid
+'''
+    *
+   ***
+  *****
+ ******* 
+'''
+n=int(input())
+for i in range(n):
+    for j in range(n-i-1): #space 
+        print(' ',end='')
+    for j in range(2*i+1): #star
+        print('*',end='')
+    for j in range(n-i-1): #space
+        print(' ',end='')
+    print()
