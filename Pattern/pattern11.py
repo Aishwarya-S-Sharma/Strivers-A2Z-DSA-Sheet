8edc144f461e47435becd3bfbcc2841c6a26803f
Aishwarya S <98091836+Aishwarya-S-Sharma@users.noreply.github.com>
2023-10-28 01:55:54 +0530

    Add files via upload

---
+++ Pattern/pattern11.py
@@ -0,0 +1,30 @@
+'''
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*********
+ ******* 
+  *****  
+   ***   
+    *   
+'''
+
+n=int(input())
+for i in range(n):
+    for j in range(n-i-1): #space 
+        print(' ',end='')
+    for j in range(2*i+1): #star
+        print('*',end='')
+    for j in range(n-i-1): #space
+        print(' ',end='')
+    print()
+for i in range(n):
+    for j in range(i): #space 
+        print(' ',end='')
+    for j in range(2*n-(2*i+1)): #star
+        print('*',end='')
+    for j in range(i): #space
+        print(' ',end='')
+    print()
